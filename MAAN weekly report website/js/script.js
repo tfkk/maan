ce8bec7cbb62c76aed5ba9595d2ffe8b8bcee5db
Filip <47536273+tfkk@users.noreply.github.com>
2022-10-15 11:59:00 +0200

    First commit
    Creating header section and adding other sections with lore

---
+++ MAAN weekly report website/js/script.js
@@ -0,0 +1,11 @@
+//script for toggling menu on mobile/tablets
+
+let navLinks = document.getElementById("navLinks");
+
+function showMenu() {
+  navLinks.style.right = "0";
+}
+
+function hideMenu() {
+  navLinks.style.right = "-200px";
+}
