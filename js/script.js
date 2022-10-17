3055a6e88a16f65b16c47a045bf73e5c4749c99b
tfkk <txficvk@gmail.com>
2022-10-17 13:49:25 +0200

    Created first weekly raport for Purchase Dept and Warehouse

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 //script for toggling menu on mobile/tablets
 
 let navLinks = document.getElementById("navLinks");
+let popup = document.getElementById("popup");
 
 function showMenu() {
   navLinks.style.display = "block";
